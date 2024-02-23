0e6967a720d1eb5130334df2e709def9a9e75f0d
mack-24 <navghanevinayak24@gmail.com>
2024-02-23 19:02:00 +0530

    javascript logic

---
+++ script.js
@@ -0,0 +1,26 @@
+      
+let input = document.getElementById("input-box")
+let listContainer = document.getElementById("list-container")
+
+function addTodo(){
+  
+  if(input.value === ''){
+    alert("Please enter a todo")
+  }else{
+  let li = document.createElement("li")  
+    li.innerHTML = input.value
+    li.innerHTML += ' <button onclick="deleteTodo(this)">delete</button>';
+    listContainer.appendChild(li)
+  document.getElementById("input-box").value = ""
+  }
+}
+
+function deleteTodo(element){
+  console.log(element)
+  element.parentNode.remove();
+}
+
+function deleteAllTodos(){
+    console.log(listContainer)
+  listContainer.remove();
+}
